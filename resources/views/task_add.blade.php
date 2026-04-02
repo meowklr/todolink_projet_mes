9a36ca6feda70d9b9410ec8e2afcdbf3753f98a8
mathieu <mathieu.morinm@gmail.com>
2026-04-02 16:28:12 +0200

    Task_add V1.2

--- resources/views/task_add.blade.php
+++ resources/views/task_add.blade.php
@@ -8,34 +8,54 @@
     <link rel="stylesheet" href="{{ asset('task_add.css') }}">
 </head>
 <body>
-    <div class="task_add">
-        <div class="partie_droite colonne">
-            <label for="nom_tache">Nom de la tache</label>
-            <input id="nom_tache" type="text" placeholder="Nom de la tache">
 
-            <label for="descriptif">Description</label>
-            <textarea id="descriptif" placeholder="Decris la tache..."></textarea>
-
-            <label for="date_limite">Date limite</label>
-            <input id="date_limite" type="date">
-        </div>
-
-        <div class="barre"></div>
-
-        <div class="partie_gauche colonne">
-            <label for="collaborateurs">Collaborateurs</label>
-            <button class="collaborateur">Ajouter les Collaborateurs</button>
-            <div class="collaborateur_present">
-                <p>kjsfjfs</p>
+<section class="page">
+        <section class="main_container">
+            <div class="navbar">
+                <a href="{{ route('home') }}" class="nav_logo"><img src="{{ asset('images/Logo_TODOLINK.png') }}" alt="Logo ToDoLink"></a>
+                <div class="navbar_elements">
+                    <a href="{{ route('home') }}">Accueil</a>
+                    <a href="{{ route('dashboard') }}">Tableau de Bord</a>
+                    <a href="{{ route('task_add') }}">Nouvelle tâche</a>
+                </div>
+                <div class="auth_buttons">
+                    <button class="btn btn--solid">Se connecter</button>
+                </div>
             </div>
+            <section class="content">
+                <div class="task_add">
+                    <div class="partie_droite colonne">
+                        <label for="nom_tache">Nom de la tache</label>
+                        <input id="nom_tache" type="text" placeholder="Nom de la tache">
 
-            <label for="piece_jointe">Ajouter fichier</label>
-            <label for="piece_jointe" class="file_button">Choisir un fichier</label>
-            <input id="piece_jointe" type="file" class="file_input">
+                        <label for="descriptif">Description</label>
+                        <textarea id="descriptif" placeholder="Decris la tache..."></textarea>
 
-            <input type="submit" value="Ajouter la tache">
-        </div>
-    </div>
+                        <label for="date_limite">Date limite</label>
+                        <input id="date_limite" type="date">
+                    </div>
+
+                    <div class="barre"></div>
+
+                    <div class="partie_gauche colonne">
+                        <label for="collaborateurs"  >Collaborateurs</label>
+                        <button class="collaborateur btn">Ajouter les Collaborateurs</button>
+                        <div class="collaborateur_present">
+                            <p>Timéo</p>
+                            <p>Nathan</p>
+                            <p>Mathieu</p>
+                        </div>
+                        
+                        <label for="piece_jointe" class="btn">Choisir un fichier</label>
+                        <input id="piece_jointe" type="file" class="file_input">
+                        <br><br><br><br><br><br>
+                        <input  class = "btn" type="submit" value="Ajouter la tache">
+                    </div>
+                </div>
+            </section>
+        </section>
+    </section>
+    
     
 </body>
 </html>
