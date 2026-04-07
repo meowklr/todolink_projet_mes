5b84a3976fcb78003aacb1a07f5312f0f83a0841
mathieu <mathieu.morinm@gmail.com>
2026-04-07 11:06:25 +0200

    Intégration d'un Favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil - ToDoLink</title>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
 </head>
 <body>
