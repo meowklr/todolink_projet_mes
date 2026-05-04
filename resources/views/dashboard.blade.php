221902eb11faced0d831e97cc651cd6ea3639c0c
Nathan <nathnath93160@gmail.com>
2026-05-04 11:03:06 +0200

    Changement nom page dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Tableau de Bord</title>
     <link rel="icon" href="{{ asset('images/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
 </head>
